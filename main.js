c3b858adc9736da8d1ec49b4500ee14b0f75021c
josiahcameron <josiahcameron2015@gmail.com>
2023-01-08 12:20:29 -0500

    pushOperator

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 (function (){
     "use strict";
 
+const $numberButton = document.querySelectorAll('[number]');
+const $operationButton = document.querySelectorAll('[operator]');
+const $equalButton = document.querySelector('[equal-sign]');
+const $plusOrMinus = document.querySelector('[plus-minus]');
 
-()})
+/*Define a function named pushNumber that alert()s the number
+associated with its event argument when called;
+add this function as an event listener for the number buttons
+*/
+button.addEventListener("click", pushNumber = () => {
+    window.alert(this.numberButton.toString);
+})
+
+/*
+Define a function named pushOperator that alert()s the
+operator (+, -, *, /, C) associated with its event argument when called;
+add this function as an event listener for the operator buttons
+*/
+button.addEventListener("click", pushOperator = () => {
+    window.alert(this.numberButton.toString);})
+})
